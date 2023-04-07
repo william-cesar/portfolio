f858b338ebdaa7510ad542ab1a4710f4ea5ff2ff
william-tome <william.tome@azion.com>
2023-04-07 18:40:12 -0300

    feat: create page links component

---
+++ src/components/app-header/components/pages-links/pages-links.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav class="pages-links">
+    <RouterLink
+      class="links"
+      to="/about"
+      title="About page"
+    >
+      About
+    </RouterLink>
+    <RouterLink
+      class="links"
+      to="/projects"
+      title="Projects page"
+    >
+      Projects
+    </RouterLink>
+    <RouterLink
+      class="links"
+      to="/contact"
+      title="Contact page"
+    >
+      Contact
+    </RouterLink>
+  </nav>
+</template>
+
+<style lang="scss">
+.pages-links {
+  display: flex;
+  gap: $spacing-32;
+
+  & > .links {
+    font-family: $font-family-2;
+    font-size: $font-size-16;
+
+    &.router-link-active,
+    &.router-link-active:hover {
+      border-bottom: $size-1 solid $secondary-color;
+      color: $secondary-color;
+    }
+  }
+}
+
+.dark .pages-links {
+  & > .links {
+    color: $primary-color-inverse;
+
+    &.router-link-active,
+    &.router-link-active:hover {
+      border-bottom-color: $primary-text-color-inverse;
+      color: $primary-text-color-inverse;
+    }
+  }
+
+  & > .links:hover {
+    color: $primary-color-hover-inverse;
+  }
+}
+</style>
