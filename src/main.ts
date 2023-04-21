67b5453aada677669f5a932dfa4b0135a2a2bda2
william-tome <william.tome@azion.com>
2023-04-21 20:52:55 -0300

    chore: config notifier plugin

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import Vue3Toastity, { type ToastContainerOptions } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(Vue3Toastity, {
+  autoClose: 4000,
+  newestOnTop: true,
+  limit: 3
+} as ToastContainerOptions);
 
 app.mount('#app');
