11bfcaef04560b9da33e3a0ce9c9a958682e9d40
william-tome <william.tome@azion.com>
2023-04-23 01:07:23 -0300

    feat: export and apply component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import AppHeader from './app-header/app-header.vue';
+import AppPhone from './app-phone/app-phone.vue';
 import PdfPreviewer from './pdf-previewer/pdf-previewer.vue';
 
-export { AppHeader, PdfPreviewer };
+export { AppHeader, AppPhone, PdfPreviewer };
