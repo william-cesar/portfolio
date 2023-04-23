e485f557c7dd4a4694a37951bb8b528e555180b0
william-tome <william.tome@azion.com>
2023-04-23 17:36:24 -0300

    feat: create date types and constants

---
+++ src/types/date-formatter.ts
@@ -0,0 +1,11 @@
+export type DateFormatter = {
+  weekday: string;
+  month: string;
+  day: number;
+  hour: number;
+  minutes: string | number;
+};
+
+export type MonthKeys = { [key: number]: string };
+
+export type WeekdayKeys = { [key: number]: string };
