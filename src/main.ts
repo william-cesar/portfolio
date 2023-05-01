b3a426b2e0e96beb26fd8d0ffaf39b5326b4c32f
william-tome <william.tome@azion.com>
2023-05-01 02:02:42 -0300

    refactor: notifier - adjust configuration

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import Vue3Toastity, { type ToastContainerOptions } from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
+import i18n from './plugins/i18n';
+import { Vue3Toastity, toastConfigOptions } from '@/plugins/notifier/config';
 
 const app = createApp(App);
 
 app.use(router);
-app.use(Vue3Toastity, {
-  autoClose: 4000,
-  newestOnTop: true,
-  limit: 3
-} as ToastContainerOptions);
+app.use(i18n);
+app.use(Vue3Toastity, toastConfigOptions);
 
 app.mount('#app');
