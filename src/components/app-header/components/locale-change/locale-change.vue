711878b024680fa0ce05543878406299fb565deb
william-tome <william.tome@azion.com>
2023-05-01 23:22:43 -0300

    feat: i18n - create language change buttons

--- src/components/app-header/components/locale-change/locale-change.vue
+++ src/components/app-header/components/locale-change/locale-change.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="locale-change">
     <button
-      class="locale-pt"
-      @click="setLocale('pt')"
-      :title="$t('navbar.pt')"
-    >
-    </button>
-    <button
       class="locale-en"
+      :class="{ 'locale-active': $i18n.locale === 'en' }"
       @click="setLocale('en')"
       :title="$t('navbar.en')"
-    >
-    </button>
+    />
+    <button
+      class="locale-pt"
+      :class="{ 'locale-active': $i18n.locale === 'pt' }"
+      @click="setLocale('pt')"
+      :title="$t('navbar.pt')"
+    />
   </div>
 </template>
 
@@ -24,6 +24,8 @@ const setLocale = (locale: 'en' | 'pt'): void => {
 </script>
 
 <style lang="scss">
+$size-minus-1: calc(-1 * $size-1);
+
 .locale-change {
   @include centralize();
   gap: $spacing-16;
@@ -33,12 +35,22 @@ const setLocale = (locale: 'en' | 'pt'): void => {
     @include circle-element($size-24);
     padding: 0;
     background: url('@/assets/files/svg/brazil.svg') -4px -5px / 40px 32px no-repeat;
+    position: relative;
   }
 
   & > .locale-en {
     @include circle-element($size-24);
     padding: 0;
     background: url('@/assets/files/svg/usa.svg') -6px -5px / 46px 32px no-repeat;
+    position: relative;
+  }
+
+  & > .locale-active:after {
+    content: '';
+    position: absolute;
+    inset: $size-minus-1;
+    border: $size-2 solid $primary-color;
+    border-radius: 50%;
   }
 }
 </style>
