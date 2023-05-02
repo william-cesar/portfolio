66815ed0f6fb268863b397e33e0b40a78a2e84d3
william-tome <william.tome@azion.com>
2023-05-01 23:22:00 -0300

    feat: i18n - add languages texts

--- src/plugins/i18n/languages/pt-BR.ts
+++ src/plugins/i18n/languages/pt-BR.ts
@@ -1,14 +1,49 @@
 export default {
+  pages: {
+    about: 'Sobre mim',
+    projects: 'Projetos',
+    blog: 'Blog',
+    home: 'Início'
+  },
   navbar: {
     en: 'English',
-    pt: 'Português'
+    pt: 'Português',
+    switch: {
+      dark: 'Alterar para modo escuro',
+      light: 'Alterar para modo claro'
+    }
   },
-  active: 'Ativo',
-  cancel: 'Cancelar',
-  go_to_doc: 'Ir para documentação',
-  list: {
-    add: 'Adicionar exemplo'
+  phone: {
+    social: {
+      linkedin: {
+        label: 'LinkedIn',
+        description: 'Visite meu perfil e veja minha carreira profissional.'
+      },
+      github: {
+        label: 'GitHub',
+        description: 'Veja meus outros projetos e como escrevo meus códigos.'
+      },
+      mail: {
+        label: 'Email',
+        description: 'Me manda uma mensagem. Vamos conversar!'
+      }
+    }
   },
-  save: 'Salvar',
-  welcome: 'Bem vindo(a)'
+  download: 'Download',
+  errors: {
+    file_download: 'O arquivo não pode ser baixado. Por favor, tente novamente mais tarde.',
+    file_render: 'Ocorreu um erro ao exibir o arquivo. Por favor, tente novamente mais tarde.'
+  },
+  presentation: {
+    name: 'Oi, eu sou William! 👋',
+    intro: {
+      p1: 'Transformar',
+      p2: 'ideias',
+      p3: 'em',
+      p4: 'produtos reais',
+      p5: 'é comigo.'
+    },
+    description:
+      'Sou um desenvolvedor (e nas horas vagas, escrevo sobre tecnologia) com experiência na criação de aplicações para a web.'
+  }
 };
