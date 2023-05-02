fc4d0300580b0103cd8b018106e863cf6ea8656d
william-tome <william.tome@azion.com>
2023-05-01 23:20:42 -0300

    feat: i18n - add datetime formatter

--- src/plugins/i18n/index.ts
+++ src/plugins/i18n/index.ts
@@ -4,10 +4,34 @@ import enUS from './languages/en-US';
 
 export default createI18n({
   legacy: false,
-  locale: 'pt',
+  locale: 'en',
   fallbacklocale: 'en',
   messages: {
     en: enUS,
     pt: ptBR
+  },
+  datetimeFormats: {
+    en: {
+      hour: {
+        timeStyle: 'short',
+        hour12: true
+      },
+      date: {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+      }
+    },
+    pt: {
+      hour: {
+        timeStyle: 'short',
+        hour12: false
+      },
+      date: {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+      }
+    }
   }
 });
