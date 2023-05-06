5add92886831316bb060f0c78ac6988d3da57377
William César <67482520+william-cesar@users.noreply.github.com>
2023-05-05 23:26:55 -0300

    feat: router - add hook to change page title (#13)
    Co-authored-by: william-tome <william.tome@azion.com>

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { beforeEachHook } from './hooks';
 import HomeView from '@/views/HomeView.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: () => import('../views/BlogView.vue')
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: () => import('../views/ProjectsView.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/'
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/blog',
-      name: 'Blog',
-      component: () => import('../views/BlogView.vue')
-    },
-    {
-      path: '/projects',
-      name: 'Projects',
-      component: () => import('../views/ProjectsView.vue')
-    }
-  ]
+  routes
 });
+
+router.beforeEach(beforeEachHook);
 
 export default router;
