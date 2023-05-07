8c2fc72d60798558700db5cbdeaa1d87c6c4d5d1
william-tome <william.tome@azion.com>
2023-05-07 00:42:55 -0300

    feat: app-tag - create and export app tag

---
+++ src/components/app-tag/app-tag.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    class="app-tag"
+    :style="{ backgroundColor: color + '77', borderColor: color }"
+  >
+    <span class="tag-label">{{ label }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  label: { type: String, default: 'Tag' },
+  color: { type: String, default: '#8e8e8e' }
+})
+</script>
+
+<style lang="scss">
+.app-tag {
+  @include centralize();
+  width: fit-content;
+  padding: $spacing-2 $spacing-8;
+  border-radius: $size-8;
+  border-width: $size-2;
+  border-style: solid;
+  user-select: none;
+
+  & > .tag-label {
+    font-size: $size-12;
+    letter-spacing: $size-1;
+    color: $primary-text-color;
+    white-space: nowrap;
+  }
+}
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 import AppHeader from './app-header/app-header.vue';
 import AppPhone from './app-phone/app-phone.vue';
+import AppTag from './app-tag/app-tag.vue';
 import PdfPreviewer from './pdf-previewer/pdf-previewer.vue';
 import WorkSection from './work-section/work-section.vue';
 
-export { AppHeader, AppPhone, PdfPreviewer, WorkSection };
+export { AppHeader, AppPhone, AppTag, PdfPreviewer, WorkSection };
