160ba1d5cfed9d3a4e4d22b4ef0c337a7769eac2
william-tome <william.tome@azion.com>
2023-05-06 21:21:22 -0300

    feat: jobs-section - create helper to provide texts

---
+++ src/components/work-section/jobs-list.ts
@@ -0,0 +1,48 @@
+import type Job from '@/types/job';
+import enUS from '@/plugins/i18n/languages/en-US';
+
+const infoList = (company: keyof typeof enUS.work_experience): string[] => {
+  const keys: string[] = Object.keys(enUS.work_experience[company].activities);
+  return keys.map((key) => {
+    return `work_experience.${company}.activities.${key}`;
+  });
+};
+
+const techList = (company: keyof typeof enUS.work_experience): string[] => {
+  const keys: string[] = Object.keys(enUS.work_experience[company].main_techs);
+  return keys.map((key) => {
+    return `work_experience.${company}.main_techs.${key}`;
+  });
+};
+
+const JobsList: Job[] = [
+  {
+    id: 'az01',
+    name: 'work_experience.azion.company',
+    companyLink: 'https://www.azion.com',
+    position: 'work_experience.azion.position',
+    period: 'work_experience.azion.period',
+    info: infoList('azion'),
+    mainTechs: techList('azion')
+  },
+  {
+    id: 'yo01',
+    name: 'work_experience.youx.company',
+    companyLink: 'https://youxgroup.com.br',
+    position: 'work_experience.youx.position',
+    period: 'work_experience.youx.period',
+    info: infoList('youx'),
+    mainTechs: techList('youx')
+  },
+  {
+    id: 'up01',
+    name: 'work_experience.uptime.company',
+    companyLink: 'https://www.instagram.com/uptimelavras/',
+    position: 'work_experience.uptime.position',
+    period: 'work_experience.uptime.period',
+    info: infoList('uptime'),
+    mainTechs: techList('uptime')
+  }
+];
+
+export default JobsList;
---
+++ src/types/job.ts
@@ -0,0 +1,11 @@
+type Job = {
+  id: string;
+  name: string;
+  companyLink: string;
+  position: string;
+  period: string;
+  info: string[];
+  mainTechs: string[];
+};
+
+export default Job;
